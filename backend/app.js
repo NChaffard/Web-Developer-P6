92b3636db04c676a9d1bf725a63306f71f7b730b
NChaffard <nchaffard@fastmail.com>
2022-06-18 21:19:57 +0200

    crud ok manque like

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const saucesRoutes = require('./routes/sauces');
+const userRoutes = require('./routes/user');
 
 const app = express();
 
+mongoose.connect('mongodb+srv://chacha:0000@cluster0.e5hshrg.mongodb.net/hotTakes?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
 app.use((req, res, next) => {
-  console.log('Requête reçue !');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
-
-
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
-});
+app.use(express.json());
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/api/sauces', saucesRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
